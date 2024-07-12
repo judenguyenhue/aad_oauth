aa41f3bf17528b9c83f3023f3cbb0531e3f6e983
judenguyenhue <jude.nguyen.hue@gmail.com>
2024-07-12 14:38:40 +0700

    - Add webview error info log

--- lib/request_code.dart
+++ lib/request_code.dart
@@ -23,13 +23,22 @@ class RequestCode {
     _navigationDelegate = NavigationDelegate(
       onNavigationRequest: _onNavigationRequest,
       onWebResourceError: (error) {
-        print('onWebResource error: ${error.description}');
+        String url = error.url ?? "";
+        if (url.isEmpty || !url.contains('http')) {
+          print('===onWebResource error: empty or non-http url load');
+        } else {
+          print('===onWebResource error: ${error.description}, url: $url');
+          showErrorMessage(
+            _config.navigatorKey.currentState?.overlay?.context,
+            'Load web source failed: ${error.description}, url: $url',
+          );
+        }
       },
       onUrlChange: (url) {
-        print('onUrlChange: ${url.url ?? ""}');
+        print('===onUrlChange: ${url.url ?? ""}');
       },
       onPageFinished: (url) {
-        print('onPageFinished: $url');
+        print('===onPageFinished: $url');
       },
     );
   }
@@ -48,7 +57,7 @@ class RequestCode {
           cookieManager.platform as AndroidWebViewCookieManager;
       androidManager.setAcceptThirdPartyCookies(
           controller.platform as AndroidWebViewController, true);
-      print('web controller accepted 3rd party cookies');
+      print('Web controller accepted 3rd party cookies');
     }
     await controller.setBackgroundColor(Colors.transparent);
     await controller.setUserAgent(_config.userAgent);
@@ -57,12 +66,17 @@ class RequestCode {
     final webView = WebViewWidget(controller: controller);
 
     if (_config.navigatorKey.currentState == null) {
-      throw Exception(
-        'Could not push new route using provided navigatorKey, Because '
-        'NavigatorState returned from provided navigatorKey is null. Please Make sure '
-        'provided navigatorKey is passed to WidgetApp. This can also happen if at the time of this method call '
-        'WidgetApp is not part of the flutter widget tree',
+      String errMessage =
+          'Could not push new route using provided navigatorKey, Because '
+          'NavigatorState returned from provided navigatorKey is null. Please Make sure '
+          'provided navigatorKey is passed to WidgetApp. This can also happen if at the time of this method call '
+          'WidgetApp is not part of the flutter widget tree';
+
+      showErrorMessage(
+        _config.navigatorKey.currentState?.overlay?.context,
+        'Load web source failed: $errMessage, url: ${_authorizationRequest.url}, params: $urlParams',
       );
+      throw Exception(errMessage);
     }
 
     await _config.navigatorKey.currentState!.push(
@@ -105,7 +119,12 @@ class RequestCode {
         _config.navigatorKey.currentState!.pop();
       }
     } catch (e) {
-      print('onNavigationRequest error: $e');
+      print(
+          '===onNavigationRequest error: $e, navigate state: ${_config.navigatorKey.currentState?.overlay?.context}');
+      showErrorMessage(
+        _config.navigatorKey.currentState?.overlay?.context,
+        'Web navigation error: ${e.toString()}, url: ${request.url}',
+      );
     }
     return NavigationDecision.navigate;
   }
@@ -127,5 +146,25 @@ class RequestCode {
     customParams.forEach((String key, String value) =>
         queryParams.add('$key=${Uri.encodeQueryComponent(value)}'));
     return queryParams.join('&');
+  }
+
+  void showErrorMessage(BuildContext? context, String text) {
+    if (context == null) {
+      return;
+    }
+    var alert = AlertDialog(
+        title: Text('Error'),
+        content: Text(text),
+        actions: <Widget>[
+          TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.pop(context);
+              })
+        ]);
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => alert,
+    );
   }
 }
